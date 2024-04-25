fd6ab49022f07f7b887c1b76e2f802d244f00ba8
ypsitau <ypsitau@nifty.com>
2024-04-25 14:07:13 +0900

    refined exp-fastest-pulse

--- exp-fastest-pulse/exp-fastest-pulse.ino
+++ exp-fastest-pulse/exp-fastest-pulse.ino
@@ -1,29 +1,17 @@
+// Generates a pulse of 4MHz (16MHz / 4clk)
 void setup()
 {
 	pinMode(2, OUTPUT);
 	noInterrupts();
-	const uint8_t maskPin2 = 1 << 2;
-	uint8_t dataHi = PORTD | maskPin2;
-	uint8_t dataLo = PORTD & ~maskPin2;
+	uint8_t dataHi = PORTD | (1 << PD2);
+	uint8_t dataLo = PORTD & ~(1 << PD2);
 	__asm__ volatile(
     "loop:"						"\n"
-	"	out %[portd],%[dataHi]"	"\n" // 1
-	"	out %[portd],%[dataLo]"	"\n" // 1
-	"	nop"					"\n"
-	"	nop"					"\n"
-	"	nop"					"\n"
-	"	nop"					"\n"
-	"	nop"					"\n"
-	"	nop"					"\n"
-	"	nop"					"\n"
-	"	nop"					"\n"
-	"	nop"					"\n"
-	"	nop"					"\n"
-	"	nop"					"\n"
-	"	nop"					"\n"
-	"	rjmp loop"				"\n" // 2
+	"	out %[portD],%[dataHi]"	"\n" // 1clk
+	"	out %[portD],%[dataLo]"	"\n" // 1clk
+	"	rjmp loop"				"\n" // 2clk
 	:
-	:	[portd] "I" (_SFR_IO_ADDR(PORTD)),
+	:	[portD] "I" (_SFR_IO_ADDR(PORTD)),
 		[dataHi] "r" (dataHi),
 		[dataLo] "r" (dataLo)
 	);
