9e8120c0b0e393b301fd92cb7166413866b4434f
ypsitau <ypsitau@nifty.com>
2024-04-28 18:55:45 +0900

    implementing exp-analog-write

--- exp-analog-write/exp-analog-write.ino
+++ exp-analog-write/exp-analog-write.ino
@@ -1,6 +1,6 @@
-void EnablePWM(int pin)
+template<int pin> void EnablePWM()
 {
-	if (pin == 3) { // PD3 is controlled by OC2B
+	if constexpr (pin == 3) { // PD3 is controlled by OC2B
 		TCCR2A |= 1 << COM2B1;
 	} else if (pin == 5) { // PD5 is controlled by OC0B
 		TCCR0A |= 1 << COM0B1;
@@ -15,9 +15,26 @@ void EnablePWM(int pin)
 	}
 }
 
-void OutputPWM(int pin, uint8_t value)
+template<int pin> void DisablePWM()
 {
-	if (pin == 3) { // PD3 is controlled by OC2B
+	if constexpr (pin == 3) { // PD3 is controlled by OC2B
+		TCCR2A &= ~(1 << COM2B1);
+	} else if (pin == 5) { // PD5 is controlled by OC0B
+		TCCR0A &= ~(1 << COM0B1);
+	} else if (pin == 6) { // PD6 is controlled by OC0A
+		TCCR0A &= ~(1 << COM0A1);
+	} else if (pin == 9) { // PB1 is controlled by OC1A
+		TCCR1A &= ~(1 << COM1A1);
+	} else if (pin == 10) { // PB2 is controlled by OC1B
+		TCCR1A &= ~(1 << COM1B1);
+	} else if (pin == 11) { // PB3 is controlled by OC2A
+		TCCR2A &= ~(1 << COM2A1);
+	}
+}
+
+template<int pin> void OutputPWM(uint8_t value)
+{
+	if constexpr (pin == 3) { // PD3 is controlled by OC2B
 		OCR2B = value;
 	} else if (pin == 5) { // PD5 is controlled by OC0B
 		OCR0B = value;
@@ -32,19 +49,19 @@ void OutputPWM(int pin, uint8_t value)
 	}
 }
 
-int pin = 11;
+constexpr int pin = 11;
 
 void setup()
 {
 
 	Serial.begin(57600);
 	pinMode(pin, OUTPUT);
-	EnablePWM(pin);
+	EnablePWM<pin>();
 }
 
 void loop()
 {
 	uint8_t value = map(analogRead(A0), 0, 1023, 0, 255);
 	Serial.println(value);
-	OutputPWM(pin, value);
+	OutputPWM<pin>(value);
 }
