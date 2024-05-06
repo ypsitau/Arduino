0ccf27347d1e75b9d09569201a06bc06fdf9b725
ypsitau <ypsitau@nifty.com>
2024-05-06 22:02:56 +0900

    reimplemtnted exp-fuse-read

--- exp-fuse-read/exp-fuse-read.ino
+++ exp-fuse-read/exp-fuse-read.ino
@@ -3,24 +3,20 @@
 
 namespace av = avrt;
 
-AVRT_IMPLEMENT_Serial0(serial)
+AVRT_IMPLEMENT_Serial0_NoRecv(serial)
 
 void setup()
 {
+	uint8_t data;
 	serial.Open(av::Serial::BaudRate57600);
-	serial.Printf("Low Fuse: %02x", boot_lock_fuse_bits_get(GET_LOW_FUSE_BITS));
-#if 0
-	Serial.println();
-	Serial.print("High Fuse: ");
-	PrintHex(boot_lock_fuse_bits_get(GET_HIGH_FUSE_BITS));
-	Serial.println();
-	Serial.print("Extended Fuse: ");
-	PrintHex(boot_lock_fuse_bits_get(GET_EXTENDED_FUSE_BITS));
-	Serial.println();
-	Serial.print("Lock: ");
-	PrintHex(boot_lock_fuse_bits_get(GET_LOCK_BITS));
-	Serial.println();
-#endif
+	data = boot_lock_fuse_bits_get(GET_LOW_FUSE_BITS);
+	serial.Printf(F("Low Fuse:      0x%02X (0b%08b)\n"), data, data);
+	data = boot_lock_fuse_bits_get(GET_HIGH_FUSE_BITS);
+	serial.Printf(F("High Fuse:     0x%02X (0b%08b)\n"), data, data);
+	data = boot_lock_fuse_bits_get(GET_EXTENDED_FUSE_BITS);
+	serial.Printf(F("Extended Fuse: 0x%02X (0b%08b)\n"), data, data);
+	data = boot_lock_fuse_bits_get(GET_LOCK_BITS);
+	serial.Printf(F("Lock:          0x%02X (0b%08b)\n"), data, data);
 }
 
 void loop()
